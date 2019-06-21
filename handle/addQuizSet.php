06b4721105fd50c05405e8f6db313cca7f9f2d03
anup8egy <anup8eguy@gmail.com>
2019-06-21 17:06:22 +0545

    v

--- handle/addQuizSet.php
+++ handle/addQuizSet.php
@@ -3,7 +3,7 @@
     session_start();
     if(isset($_POST['submit-set'])){
         if(isset($_POST['quizSetName']) && !empty($_POST['quizSetName'])){
-
+            $
         }else{
             $_SESSION['error_quiz_set']='Please enter a quiz set';
             header('location:../manage_quiz.php');
