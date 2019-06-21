9796354f6c7c0b80db7f17814f01e774b08214e4
anup8egy <anup8eguy@gmail.com>
2019-06-21 14:10:53 +0545

    quiz can be added in database new backends and sql

--- handle/addquiz.php
+++ handle/addquiz.php
@@ -10,28 +10,28 @@ if(isset($_POST['question']) && !empty($_POST['question'])){
     exit();
 }
 if(isset($_POST['answer'])&&!empty($_POST['answer'])){
-    $question=mysqli_real_escape_string($sql_connect,$_POST['answer']);
+    $answer=mysqli_real_escape_string($sql_connect,$_POST['answer']);
 }else{
     $_SESSION['error_add_quiz']='Please fill all details';
         header('location:../manage_quiz.php');
     exit();
 }
 if(isset($_POST['opt1'])&&!empty($_POST['opt1'])){
-    $question=mysqli_real_escape_string($sql_connect,$_POST['opt1']);
+    $opt1=mysqli_real_escape_string($sql_connect,$_POST['opt1']);
 }else{
     $_SESSION['error_add_quiz']='Please fill all details';
         header('location:../manage_quiz.php');
     exit();
 }
 if(isset($_POST['opt2'])&&!empty($_POST['opt2'])){
-    $question=mysqli_real_escape_string($sql_connect,$_POST['opt2']);
+    $opt2=mysqli_real_escape_string($sql_connect,$_POST['opt2']);
 }else{
     $_SESSION['error_add_quiz']='Please fill all details';
         header('location:../manage_quiz.php');
     exit();
 }
 if(isset($_POST['opt3'])&& !empty($_POST['opt3'])){
-    $question=mysqli_real_escape_string($sql_connect,$_POST['opt3']);
+    $opt3=mysqli_real_escape_string($sql_connect,$_POST['opt3']);
 }else{
     $_SESSION['error_add_quiz']='Please fill all details';
         header('location:../manage_quiz.php');
@@ -51,6 +51,22 @@ if(isset($_POST['opt3'])&& !empty($_POST['opt3'])){
 //         // header('location:../manage_quiz.php');
 //     exit();
 // }
+    if(strnatcasecmp($opt1,$opt2)==0 ||strnatcasecmp($opt1,$opt3)==0 || strnatcasecmp($opt2,$opt3)==0){
+        $_SESSION['error_add_quiz']='Options cannot be same';
+        header('location:../manage_quiz.php');
+        exit();
+    }
+    if(strnatcasecmp($answer,$opt1)==0 || strnatcasecmp($answer,$opt2)==0 || strnatcasecmp($answer,$opt3)==0){
+        //Ntg to do
+    }else{
+        $_SESSION['error_add_quiz']='One of your option should match with answer';
+        header('location:../manage_quiz.php');
+        exit();
+    }
+    unset($_SESSION['error_add_quiz']);
+    $userid=$_COOKIE['userid'];
+    $insert_data="INSERT INTO quiz(id,question,answer,opt1,opt2,opt3)VALUES('$userid','$question','$answer','$opt1','$opt2','$opt3')";
+    mysqli_query($sql_connect,$insert_data);    
 }else{
     header('location:../manage_quiz.php');
 }
