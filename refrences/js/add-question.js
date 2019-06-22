8f2d00f69e7055dfe80c61e4a1dc599abbb1a498
anup8egy <anup8eguy@gmail.com>
2019-06-22 14:37:32 +0545

    just chekc

---
+++ refrences/js/add-question.js
@@ -0,0 +1,19 @@
+function addquizquestion(){
+    var  question=document.getElementById('question').value;
+    var answer=document.getElementById('answer').value;
+    var opt1=document.getElementById('opt1').value;
+    var opt2=document.getElementById('opt2').value;
+    var opt3=document.getElementById('opt3').value;
+    var submit_check=document.getElementById('submit_question').value;
+    var datahold='question='+question + '&answer='+answer + '&opt1='+opt1 + '&opt2='+opt2 + '&opt3='+opt3 + '&submit_check='+submit_check;
+    $.ajax({
+        type:'POST',
+        url:'handle/addquiz.php',
+        data:datahold,
+        cache:false,
+        success:function(html){
+            $('.errorMsgForm').html(html);
+        }
+    })
+    return false;
+}
