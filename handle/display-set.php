676f8f69669f321532dc68a361aeff708e5174ca
yubrajbhandari923 <yubrajbhandari32@yahoo.com>
2019-06-22 13:42:47 +0545

    added  manage-quizset.html

--- handle/display-set.php
+++ handle/display-set.php
@@ -5,8 +5,10 @@
     $query_table_set=mysqli_query($sql_connect,$select_table);
     while($row=mysqli_fetch_assoc($query_table_set)){
       echo "<li>
-                    <div>".$row['setname']."
-                     <button id='manageQSbutt' class='manageQSbutt'>Manage</button>
+                    <div>".$row['setname']." :::: FieldName_here
+
+                    <button id='manageQSbutt' class='manageQSbutt'>Manage</button>
+
                      <button class='addQbutt'>Add Question</button>
                       <a href='handle/remove-set.php?set=".$row['setname']."'><button class='removeThisQSbutt'>Remove this Set</button></a>
                     </div>
