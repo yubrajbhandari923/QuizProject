11400f7182ac289a146e50509fb72a5e8adef668
yubrajbhandari923 <yubrajbhandari32@yahoo.com>
2019-06-22 08:05:31 +0545

    added removeThisQs butt in managequiz

--- handle/display-set.php
+++ handle/display-set.php
@@ -5,7 +5,8 @@
     $query_table_set=mysqli_query($sql_connect,$select_table);
     while($row=mysqli_fetch_assoc($query_table_set)){
       echo "<li>
-                    <div>".$row['setname'] ." <button id='manageQSbutt' class='manageQSbutt'>Manage</button><button class='addQbutt'>Add Question</button></div>
+                    <div>".$row['setname'] ." <button id='manageQSbutt' class='manageQSbutt'>Manage</button><button class='addQbutt'>Add Question</button>  <button class='removeThisQSbutt'>Remove this Set</button>
+                    </div>
                 </li>
                 ";
     }
--- refrences/js/open-close.js
+++ refrences/js/open-close.js
@@ -1,5 +1,4 @@
 window.onload = function(){
-
     data ={
         UIB : true
     }
