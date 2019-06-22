8c919be9356618b3d0aba81cba037be88e92137a
yubrajbhandari923 <yubrajbhandari32@yahoo.com>
2019-06-22 10:49:58 +0545

    add styling to myaccount.php

--- my_account.php
+++ my_account.php
@@ -10,15 +10,15 @@ include 'handle/user_profile.php';
         <title> Guffadi </title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel=stylesheet href="refrences/fonts/fonts.css ">
-        <link rel=stylesheet href='refrences/css/user_home-style.css'>
+        <link rel=stylesheet href='refrences/css/myaccount-style.css'>
         <link rel=stylesheet href='refrences/css/header.css'>
         <script src='refrences/js/open-close.js'>    </script>
     </head>
     <body>
     <?php include 'header.php'; ?>
         <div>
-            <div>
-               <span><h2>Full Name:</h2> <h3> <?php echo $user_name; ?></h3> </span>
+            <div class="data-container">
+               <span><h2>Full Name:</h2> <h3> <?php echo $user_name; ?></h3> <a href="#"> Edit </a></span>
                <span><h2>User Name:</h2> <h3> <?php echo $user_username;?></h3> </span>
                <span><h2>Points earned:</h2> <h3> <?php echo $points;?></h3> </span>
                <span><h2>Global Rank:</h2> <h3><?php echo $global_rank;?></h3> </span>
