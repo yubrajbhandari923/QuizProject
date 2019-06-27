8372428798a9128b610d3c2a0167c6019e63471d
yubrajbhandari923 <yubrajbhandari32@yahoo.com>
2019-06-27 16:25:07 +0545

    skeleton HTML for take_test Landing page

--- manage_quiz.php
+++ manage_quiz.php
@@ -4,6 +4,8 @@ include 'handle/afterlog.php';
 <!DOCTYPE Html>
 <html>
     <head>
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width initial-scale=1.0">
         <title> Guffadi</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel=stylesheet href="refrences/fonts/fonts.css ">
