dbe6acddbf21065ef73ff7de4e78891ef6121e39
yubrajbhandari923 <yubrajbhandari32@yahoo.com>
2019-07-09 15:34:24 +0545

    new new updates

---
+++ refrences/js/take-test-main-script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",function(){
+    let data = { i: 1}
+    var a =  setInterval(function(){
+        document.querySelector("#readyText span").innerHTML = data.i;
+        if(data.i==4){document.querySelector(".cover").style.display= "none";clearInterval(a)} 
+        data.i++;
+    },1000)
+    
+})
