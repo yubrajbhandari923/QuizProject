be969abe3a165f5f2a1690e9e011bd346d3321dc
anup8egy <anup8eguy@gmail.com>
2019-07-22 22:12:28 +0545

    errores and security updated

--- handle/sql-connection.php
+++ handle/sql-connection.php
@@ -2,7 +2,7 @@
 if(!defined('sql-connection_check')){
     include 'error.php';
     exit();
-}
+    }
 $sql_connect=mysqli_connect('localhost','root');
 $db_connect=mysqli_select_db($sql_connect,'user_record');
 mysqli_query($sql_connect,$db_connect);
