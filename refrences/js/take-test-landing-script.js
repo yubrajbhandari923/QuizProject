29278cf5df675037b56410e761b25873a609d2a8
yubrajbhandari923 <yubrajbhandari32@yahoo.com>
2019-08-03 03:24:38 -0700

    take_test.php completed stylings

--- refrences/js/take-test-landing-script.js
+++ refrences/js/take-test-landing-script.js
@@ -1,10 +1,40 @@
 document.addEventListener("DOMContentLoaded",function(){
-    let a = document.querySelectorAll("input[type=radio]") 
-    console.log(a);
+    document.querySelector("#Done-select").onclick =  function(){
+		aa = document.querySelectorAll(".btn-clicked .btn-value")
+		b = "";
+		for(i=0;i<aa.length;i++){
+			b += aa[i].innerText + " "
+		}
+		document.getElementById("field_select").innerText = b;
+		document.querySelector("input[name=field_select]").value = b;
+		document.getElementById("duration").innerText = "20s";
+		document.querySelector("#cover").style.display = "block";
+	}
+	document.querySelector("select[name=level]").addEventListener("click",function(){
+		ab = this.value;
+		if(ab=="easy"){
+			document.getElementById("duration").innerText = "20s";
+		}else if(a=="medium"){
+			  document.getElementById("duration").innerText = "15s";
+			}else{
+				document.getElementById("duration").innerText = "8s";
+		}
+	})
+	a = document.querySelectorAll(".btn")
+	for(i=0;i<a.length;i++){
+		a[i].addEventListener("click",function(){
+			if(this.className.indexOf("btn-clicked")==-1){
+				this.className += " btn-clicked";
+			}else{
+				this.className ="btn";
+			}
+		})
+	}
+	document.getElementById("cancel-butt").addEventListener("click",function(){
+		document.querySelector("#cover").style.display = "none";
+	})
 })
-function flipIt(){
-    document.querySelector("#flip-scene").className = "isflip";
-}
-function unFlipIt(){
-    document.querySelector("#flip-scene").className = "";
+function c(){
+	document.getElementById("Done-select").click();
+	return "True"
 }
